906422da9c492a3862ccf9001351ea5cde0c2cea
Teapack1 <ondrej.major@gmail.com>
2023-12-29 17:19:42 +0100

    update

--- html5up-read-only/assets/js/parallax.js
+++ html5up-read-only/assets/js/parallax.js
@@ -7,10 +7,10 @@ function parallaxEffect() {
     const scrollTop = window.pageYOffset;
   
     // Calculate the parallax scroll speed (adjust the value 0.5 to change the speed)
-    const parallaxSpeed = scrollTop * -0.05;
+    const parallaxSpeed = scrollTop * -0.04;
   
     // Apply the background-position transformation to the parallax container
-    headerElement.style.backgroundPosition = `right ${parallaxSpeed}px`;
+    headerElement.style.backgroundPosition = `center ${parallaxSpeed}px`;
   }
   
   // Add the event listener for the scroll event
